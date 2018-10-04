7df965cef080003885caf346413766e383c80388
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2018-10-04 09:57:18 -0700

    done

--- rockPaperScissors/src/Main.java
+++ rockPaperScissors/src/Main.java
@@ -3,41 +3,63 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        //creates random number generator
         Scanner userInput = new Scanner(System.in);
         //creates scanner for user input
-        System.out.println("Rock, Paper, Scissors");
-        //title
-        String userChoice = userInput.nextLine();
-        //gets input from user from next line
-        int randomNum = random.nextInt(3);
-        //computer chooses number from 3 different number and stores in randomNum
-        String computerChoice;
-        //creates variable computerChoice
+        String userChoice = null;
+        //stores user input
+        int userScore = 0;
+        int computerScore = 0;
 
-        if (randomNum == 1){
-            computerChoice = "rock";
-        }else if (randomNum == 2){
-            computerChoice = "paper";
-        }else {
-            computerChoice = "scissors";
-        } //sets each random number generated to a string
+        do {
+            System.out.println("Rock, Paper, Scissors");
+            System.out.println("Please type \"rock\", \"paper\", \"scissors\" or \"exit\"");
+            System.out.println("User: " + userScore +"\tComputer: " + computerScore);
+            System.out.println("--------------------------------------------");
+            //title
+            userChoice = userInput.nextLine().toLowerCase();
+            //gets user input from next line
+            Random random = new Random();
+            //creates random number generator
+            int randomNum = random.nextInt(3);
+            //computer chooses number from 3 different number and stores in randomNum
+            String computerChoice;
+            //creates variable computerChoice
 
-        if(userChoice == computerChoice){
-            System.out.println("Tie, you both chose " + userChoice);
-        }else if(userChoice.equals("scissors") && computerChoice.equals("paper")){
-            System.out.println("You won, the computer chose " + computerChoice);
-        }else if(userChoice.equals("scissors") && computerChoice.equals("rock")){
-            System.out.println("You lost, the computer chose " + computerChoice);
-        }else if(userChoice.equals("paper") && computerChoice.equals("rock")){
-            System.out.println("You win, the computer chose " + computerChoice);
-        }else if(userChoice.equals("paper") && computerChoice.equals("scissors")){
-            System.out.println("You lost, the computer chose " + computerChoice);
-        }else if(userChoice.equals("rock") && computerChoice.equals("scissors")){
-            System.out.println("You won, the computer chose " + computerChoice);
-        }else if(userChoice.equals("rock") && computerChoice.equals("paper")){
-            System.out.println("You lost, the computer chose " + computerChoice);
-        }
+            if (randomNum == 1) {
+                computerChoice = "rock";
+            } else if (randomNum == 2) {
+                computerChoice = "paper";
+            } else {
+                computerChoice = "scissors";
+            } //sets each random number generated to a string
+
+            if((!userChoice.equals("paper"))&&(!userChoice.equals("rock")) && (!userChoice.equals("scissors")) && (!userChoice.equals("exit"))){
+                System.out.println("Please type \"rock\", \"paper\", \"scissors\" or \"exit\"");
+            }
+
+            if (userChoice.equals(computerChoice)) {
+                System.out.println("Tie, you both chose " + userChoice);
+                userScore++;
+                computerScore++;
+            } else if (userChoice.equals("scissors") && computerChoice.equals("paper")) {
+                System.out.println("You won, the computer chose " + computerChoice);
+                userScore++;
+            } else if (userChoice.equals("scissors") && computerChoice.equals("rock")) {
+                System.out.println("You lost, the computer chose " + computerChoice);
+                computerScore++;
+            } else if (userChoice.equals("paper") && computerChoice.equals("rock")) {
+                System.out.println("You win, the computer chose " + computerChoice);
+                userScore++;
+            } else if (userChoice.equals("paper") && computerChoice.equals("scissors")) {
+                System.out.println("You lost, the computer chose " + computerChoice);
+                computerScore++;
+            } else if (userChoice.equals("rock") && computerChoice.equals("scissors")) {
+                System.out.println("You won, the computer chose " + computerChoice);
+                userScore++;
+            } else if (userChoice.equals("rock") && computerChoice.equals("paper")) {
+                System.out.println("You lost, the computer chose " + computerChoice);
+                computerScore++;
+            }
+        }while(!userChoice.equals("exit"));
     }
 }
