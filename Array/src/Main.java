758ee1027ea4ec4ecd6c3c7694cad5d0003bea10
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2018-11-19 10:59:21 -0800

    f

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,30 +1,54 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList nums = new ArrayList();
-    static Scanner input = new Scanner(System.in);
 
-    public static void addValues(int a){
-        nums.add(a);
+    public static void addValue(int[] array, int value){
+        int[] addedValues = new int[array.length + 1];
+        addedValues[addedValues.length -1] = value;
+        for (int i = 0; i < array.length;){
+            addedValues[i] = array[i];
+            i++;
+        }
+        System.out.println(Arrays.toString(addedValues));
     }
 
-    public static void deleteValues(ArrayList a){
-        a.remove(a.size() - 1);
+    public static void deleteValue(int[] array){
+        int[] deletedValue = new int[array.length - 1];
+        int poppedValue = array[array.length -1];
+        for (int i = 0; i < deletedValue.length;){
+            deletedValue[i] = array[i];
+            i++;
+        }
+        System.out.println(Arrays.toString(deletedValue));
+        System.out.println("Popped Value: "+ poppedValue);
     }
 
-    public static void insertValues(ArrayList a, int pos, int value){
-        a.add(pos,value);
+    public static void insertValue(int[] array, int pos, int value){
+        int[] insertedValue = new int[array.length + 1];
+        insertedValue[pos] = value;
+        for(int i = 0; i < array.length;){
+            if(i >= pos){
+                insertedValue[i + 1] = array[i];
+            }else{
+                insertedValue[i] = array[i];
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(insertedValue));
     }
 
     public static void main(String[] args) {
-        nums.add(input.nextInt());
-        nums.add(input.nextInt());
-        nums.add(input.nextInt());
-        nums.add(input.nextInt());
-        //addValues(2);
-        //deleteValues(nums);
-        insertValues(nums, 2,9);
-        System.out.println(nums);
+        Scanner input = new Scanner(System.in);
+        int[] nums = new int[4];
+        for (int i = 0; i < nums.length;){
+            nums[i] = input.nextInt();
+            i++;
+        }
+        System.out.println(Arrays.toString(nums));
+        //addValue(nums, 3);
+        //deleteValue(nums);
+        //insertValue(nums,2,9);
+
     }
 }
