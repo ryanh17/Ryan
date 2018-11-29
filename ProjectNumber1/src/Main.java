f408fc15e427f6780b35841f5c810ebb099c9893
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2018-11-29 11:31:36 -0800

    c

--- ProjectNumber1/src/Main.java
+++ ProjectNumber1/src/Main.java
@@ -35,30 +35,39 @@ public class Main {
     //Code your solution to problem number 3 here
     static String problemThree(String s){
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        String answer = "";
+        ArrayList answer = new ArrayList();
         ArrayList alphaList = new ArrayList();
         ArrayList characters = new ArrayList();
         int x = 0;
         int n = 0;
         for(int i = 0; i<alphabet.length();){
             char a = alphabet.charAt(i);
-            x =  (int)a;
+            x =  a;
             alphaList.add(x);
             i++;
         }
         for (int i = 0; i<s.length();){
             char y = s.charAt(i);
-            n++;
-            characters.add(y);
+            n = y;
+            characters.add(n);
             i++;
         }
-        for(int i = 0; i<s.length();){
-            if ((int)characters.get(i)<(int)characters.get(i +1)){
-               answer = answer + characters.get(i);
+        System.out.println(alphaList);
+        System.out.println(characters);
+        System.out.println(characters.size());
+        for(int i = 0; i<s.length() - 1;) {
+            if ((int)characters.get(i) <= (int)characters.get(i + 1)) {
+                answer.add(characters.get(i));
+                answer.add(characters.get(i + 1));
             }
             i++;
         }
-        return answer;
+        for (int i = 0; i<answer.size();){
+            char k = (char)(int)answer.get(i);
+            System.out.println(k);
+            i++;
+        }
+        return answer.toString();
     }
     public static void main(String[] args) {
         /*
@@ -71,6 +80,7 @@ public class Main {
         String s = "azcbobobegghakl";
         //System.out.println("Number of vowels: " + problemOne(s));
         //System.out.println("Number of times bob occurs is: " + problemTwo(s));
+        System.out.println('a' > 'z');
         System.out.println(problemThree(s));
     }
 }
