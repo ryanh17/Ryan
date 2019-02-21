6b03555e3aadf309ed813efa8417ddb8d1bd1ea4
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2019-02-21 13:38:09 -0800

    21

--- jUnit/src/IntegerSet.java
+++ jUnit/src/IntegerSet.java
@@ -1,19 +1,51 @@
 import java.util.ArrayList;
 
 public class IntegerSet {
-    ArrayList numbers;
+    ArrayList numbers = new ArrayList();
 
     //Requires: int
     //Modifies: this
     //Effects: inserts integer into set unless its already there, in which case do nothing
     public void insert (Integer num){
-        numbers.add(num);
+        if (contains(num) == false){
+            numbers.add(num);
+        }
     }
 
     //Requires: int
     //Modifies: this
-    //Effects:
+    //Effects: if the integer is in the set remove it, otherwise do nothing
     public void remove (Integer num){
-        numbers.remove(num);
+        if(contains(num) == true){
+            for(int x = 0; x <= numbers.size() - 1; x++){
+                if (numbers.get(x).equals(num)){
+                    numbers.remove(x);
+                }
+            }
+        }
+    }
+
+    //Requires: null
+    //Modifies: this
+    //Effects: return numbers ArrayList size
+    public int size() {
+        return numbers.size();
+    }
+
+    //Requires: int
+    //Modifies: this
+    //Effects: if set contains the integer, return true, otherwise return false
+    public boolean contains(int i) {
+        for (int x = 0; x <= numbers.size() - 1; x++){
+            if (numbers.get(x).equals(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 }
