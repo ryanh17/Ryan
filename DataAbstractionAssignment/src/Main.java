206cecdff585f24eb8fac1a944bbf53ee58d04bd
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2019-04-16 14:57:34 -0700

    1

---
+++ DataAbstractionAssignment/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        Customer ryan = new Customer("Ryan", 123, 300, 200);
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
