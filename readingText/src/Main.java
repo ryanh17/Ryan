af628c734d7de7b9c21d2611fb37c6972e6fa17c
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2019-06-14 13:55:01 -0700

    done

--- readingText/src/Main.java
+++ readingText/src/Main.java
@@ -6,47 +6,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static ArrayList sentences = new ArrayList();
-    //static String blah = "";
     public static void main(String[] args) throws IOException {
         FileReader fr = new FileReader("ProgrammingHistory.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
 
         while((line = br.readLine()) != null){
-            //blah += line;
             sentences.add(line);
         }
 
-        //String temp = "";
-        /*for (int i = 0; i < blah.length(); i++){
-            if (blah.charAt(i) != '.' && blah.charAt(i + 1) != '['){
-                temp += blah.charAt(i);
-            } else if(blah.charAt(i + 1) == '['){
-                temp = temp + ".[" + blah.charAt(i+2) + "]";
-                sentences.add(temp);
-                i = i + 3;
-            }else{
-                temp += blah.charAt(i);
-                sentences.add(temp);
-            }
-        }*/
-        search("punch");
-        System.out.println(sentences.get(0));
+        search("them");
     }
 
     static void search(String word){
         for(int i = 0; i<sentences.size(); i++){
             String temp = sentences.get(i).toString();
             if (temp.contains(word)){
-                System.out.println("Index Position: " + i + " " + temp.indexOf(word));
-            }
-        }
-    }
-
-    static void whereInSentence(String sentence){
-        for (int i = 0; i < sentence.length(); i++){
-            if (sentence.charAt(i) != ' ' || sentence.charAt(i) != '.'){
-
+                System.out.println("Index Position: " + i + " Index Pos in Sentence: " + temp.indexOf(word));
             }
         }
     }
