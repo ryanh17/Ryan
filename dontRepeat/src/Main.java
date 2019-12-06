baeffd342e12989217d0249269e826ba8e12d5f2
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2019-12-06 09:58:45 -0800

    777

--- dontRepeat/src/Main.java
+++ dontRepeat/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Main {
+    final static ArrayList<Integer> DAYS_PER_MONTH = new ArrayList<>(Arrays.asList(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31));
     /**
      *Finds day of the year
      * @param month integer from 1-12
@@ -7,36 +10,24 @@ public class Main {
      * @param year positive integer
      * @return dayOfMonth
      */
-    public static int dayOfYear(int month, int dayOfMonth, int year) {
-        if (month == 2) {
-            dayOfMonth += 31;
-        } else if (month == 3) {
-            dayOfMonth += 59;
-        } else if (month == 4) {
-            dayOfMonth += 90;
-        } else if (month == 5) {
-            dayOfMonth += 120;
-        } else if (month == 6) {
-            dayOfMonth += 151;
-        } else if (month == 7) {
-            dayOfMonth += 181;
-        } else if (month == 8) {
-            dayOfMonth += 212;
-        } else if (month == 9) {
-            dayOfMonth += 243;
-        } else if (month == 10) {
-            dayOfMonth += 273;
-        } else if (month == 11) {
-            dayOfMonth += 304;
-        } else if (month == 12) {
-            dayOfMonth += 335;
+    public static String dayOfYear(int month, int dayOfMonth, int year) {
+        int dayOfYear = dayOfMonth;
+
+        if(month >= 1 && month <= 12 && dayOfMonth >= 1 && year > 1){
+            if (dayOfMonth<= DAYS_PER_MONTH.get(month-1)) {
+                for (int i = 1; i < month; i++) {
+                    dayOfYear += DAYS_PER_MONTH.get(i - 1);
+                }
+            }
+        }else{
+            return "You entered invalid number";
         }
-        return dayOfMonth;
+        return String.valueOf(dayOfYear);
     }
 
 
 
     public static void main(String[] args) {
-        System.out.println(dayOfYear(3, 20, 2019));
+        System.out.println(dayOfYear(12, 31, 2019));
     }
 }
