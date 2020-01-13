c03225b74372e4895a70af511fc9c94cdcf0cdf1
ryanh17 <43790407+ryanh17@users.noreply.github.com>
2020-01-13 09:46:02 -0800

    sss

---
+++ CreateTestDocumentation/Tests/ListHolderTests.java
@@ -0,0 +1,16 @@
+public class ListHolderTests {
+    /*
+    * Testing Strategy
+    *
+    * Test all lists are holding type Type - T Object
+    * Test when list: Combined, discarded, mutated
+    * Test when listsOfLists: <2, =2,  >2
+    * */
+
+    /*
+    * Test method "combinedFirstTwo"
+    *
+    * Covers listsOfLists: <2, =2,  >2
+    * When listOfLists >2 or =2 then test second list has been removed from listOfLists
+    * */
+}
